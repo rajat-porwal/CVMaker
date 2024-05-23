5a31400162d3f6489445e41540e55031d344775b
Rajat Porwal <rajatporwal@protonmail.com>
2024-05-23 23:58:49 +0530

    add enable disable submit button logic

--- src/generalInfoSection.jsx
+++ src/generalInfoSection.jsx
@@ -1,25 +1,31 @@
 import { useState } from "react";
-function SubmitBtn({firstName, lastName, email}){
-    const [disabled, setDisabled] = useState(false);
+function SubmitBtn({firstName, lastName, email,status ,setStatus}){
+    // const [disabled, setDisabled] = useState(false);
     const submitAction = () => {
-        setDisabled(true);
         const fullName = `${firstName} ${lastName}`;
         const paragraph = document.createElement("p");
-        paragraph.textContent = `Name: ${fullName}, Email: ${email}`;g;
+        paragraph.textContent = `Name: ${fullName}, Email: ${email}`;
         document.getElementById("CVHtml").appendChild(paragraph);
-        const inputs = document.querySelectorAll("input[type='text']");
+        const inputs = document.querySelectorAll("input");
         inputs.forEach(input => input.classList.add("invisible"));
-
+        setStatus(true);
     };
     return(
-        <button onClick={submitAction} disabled= {disabled}>Submit</button>
+        <button onClick={submitAction} disabled= {status}>Submit</button>
     );
 }
 
-function EditBtn(){
+/*used status to enable disable submit button
+passed status and setstatus to submitbtn function and passes btndisabler to edit butn
+it was causing error cuz i didnt passed it as {BtnDisabler}
+*/
+
+function EditBtn({BtnDisabler}){ 
     const EditAction = () => {
-        const inputs = document.querySelectorAll("input[type='text']");
+        document.getElementById("CVHtml").innerHTML = '';
+        const inputs = document.querySelectorAll("input");
         inputs.forEach(input => input.classList.remove("invisible"));
+        BtnDisabler();
     };
     return(
         <button onClick={EditAction}>Edit</button>
@@ -32,18 +38,21 @@ export function GeneralInfoSection() {
         lastName:'',
         email: '',
     });
-    
+    const [status, setStatus] = useState(false)
+    const BtnDisabler = () => {
+        setStatus(false);
+    }
     return(
         <div>
            <h1>Please Input General Info </h1> 
 
            <input type="text" placeholder="First Name" value={name.firstName} onChange={((e) => setname({...name, firstName: e.target.value}))} />
            <input type="text" placeholder="Last Name" value={name.lastName} onChange={((e) => setname({...name, lastName:e.target.value}))} />
-           <input type="email"  placeholder="Email"value={name.email}onChange={e => setname({ ...name, email: e.target.value })}
-      />
-      <SubmitBtn firstName={name.firstName} lastName={name.lastName} email={name.email} />
-      <EditBtn />
-      <div id="CVHtml"></div>
+           <input type="email"  placeholder="Email"value={name.email}onChange={e => setname({ ...name, email: e.target.value })}/>
+           <SubmitBtn firstName={name.firstName} lastName={name.lastName} email={name.email} status = {status}  setStatus = {setStatus}/>
+           <div id="CVHtml"></div>
+           <EditBtn BtnDisabler = {BtnDisabler}/>
+      
     </div>
     );
 }
