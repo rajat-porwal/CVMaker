d41986a6218c951c030dc65e7d13a96033525c41
Rajat Porwal <rajatporwal@protonmail.com>
2024-05-24 04:32:31 +0530

    import the newly created components in components.jsx and add them to CVForm function

--- src/components/components.jsx
+++ src/components/components.jsx
@@ -1,14 +1,26 @@
-import GeneralInfo from './generalInfoSection';
-import EducationExperience from './education';
-import PracticalExperience from './experience';
+import GeneralInfo from './GeneralInfo';
+import EducationExperience from './EducationExperience';
+import PracticalExperience from './PracticalExperience';
+import ProfessionalSkills from './ProfessionalSkills';
+import PersonalSkills from './PersonalSkills';
+import Languages from './Languages';
+import ContactInfo from './ContactInfo';
+import SocialLinks from './SocialLinks';
+import About from './About';
 
 const CVForm = () => {
   return (
-    <div>
+    <div className="cv-form">
       <h1>CV Form</h1>
       <GeneralInfo />
+      <About />
       <EducationExperience />
       <PracticalExperience />
+      <ProfessionalSkills />
+      <PersonalSkills />
+      <Languages />
+      <ContactInfo />
+      <SocialLinks />
     </div>
   );
 };
