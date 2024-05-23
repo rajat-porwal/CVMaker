5a929877be15c8e9f5882c350f4fd059a37f1e2a
Rajat Porwal <rajatporwal@protonmail.com>
2024-05-24 01:31:04 +0530

    add edit button click part

--- src/education.jsx
+++ src/education.jsx
@@ -17,4 +17,22 @@ const EducationExperience = ({ education, onSubmit, onEdit }) => {
         onEdit();
     };
 
+    return (
+        <div>
+          {isEditing ? (
+            <form onSubmit={handleSubmit}>
+              <input type="text" value={school} onChange={(e) => setSchool(e.target.value)} placeholder="School Name" />
+              <input type="text" value={title} onChange={(e) => setTitle(e.target.value)} placeholder="Title of Study" />
+              <input type="text" value={date} onChange={(e) => setDate(e.target.value)} placeholder="Date of Study" />
+              <button type="submit">Submit</button>
+            </form>
+          ) :(
+            <div>
+              <p>School Name: {school}</p>
+              <p>Title of Study: {title}</p>
+              <p>Date of Study: {date}</p>
+              <button onClick={handleEdit}>Edit</button>
+            </div>
+          )}
+        </div>
 
